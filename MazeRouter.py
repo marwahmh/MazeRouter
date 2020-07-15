9eeefa173da117c6ee3d4560a81353bdca75c162
marwahmh <52168271+marwahmh@users.noreply.github.com>
2020-07-15 22:30:42 +0200

    reading input file

---
+++ MazeRouter.py
@@ -0,0 +1,15 @@
+def MazeRouter(inFile):
+
+    #reading input file
+    infile=open(inFile)
+    content=infile.read()
+    infile.close()
+    nets=content.split('\n')
+    print(nets)
+
+
+
+
+    
+
+MazeRouter('input file.txt')
